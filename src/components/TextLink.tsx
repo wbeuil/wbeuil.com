b05a8f8d9fc69a7c671a2b48921c32dddd3eb66f
William Beuil <william.beuil@gmail.com>
2021-05-22 20:30:00 +0200

    External link in a blog should open a new tab

--- src/components/TextLink.tsx
+++ src/components/TextLink.tsx
@@ -1,16 +1,41 @@
+import NextLink from 'next/link';
+
+import Anchor from 'components/Anchor';
+
 type TextLinkProps = {
   href: string;
 };
 
 const TextLink: React.FC<TextLinkProps> = ({ href, children, ...props }) => {
-  return (
-    <a
-      className='text-blue-400 rounded hover:underline focus:outline-none focus:ring-2'
-      href={href}
-      {...props}>
-      {children}
-    </a>
-  );
+  const classes =
+    'text-blue-400 rounded hover:underline focus:outline-none focus:ring-2';
+
+  if (href?.match(/^#/)) {
+    return (
+      <a className={classes} href={href} {...props}>
+        {children}
+      </a>
+    );
+  } else if (href?.match(/(^http)/i)) {
+    return (
+      <a
+        className={classes}
+        href={href}
+        target='_blank'
+        rel='noopener noreferrer'
+        {...props}>
+        {children}
+      </a>
+    );
+  } else {
+    return (
+      <NextLink href={href} passHref>
+        <Anchor className={classes} {...props}>
+          {children}
+        </Anchor>
+      </NextLink>
+    );
+  }
 };
 
 export default TextLink;
