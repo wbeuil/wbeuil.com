8eb9e7e17825ca392e8f07bc1cd96213731f71f8
William Beuil <william.beuil@gmail.com>
2021-06-05 11:56:30 +0200

    Animation for navbar and menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,15 +27,6 @@ module.exports = {
       borderColor: {
         primary: 'var(--color-border-primary)',
       },
-      keyframes: {
-        'slide-up': {
-          '0%': { transform: 'translateY(0.5rem)', opacity: 0 },
-          '100%': { transform: 'translateY(0)', opacity: 1 },
-        },
-      },
-      animation: {
-        'slide-up': 'slide-up 250ms cubic-bezier(0.4, 0, 0.6, 1)',
-      },
       zIndex: {
         '-10': '-10',
         '-20': '-20',
